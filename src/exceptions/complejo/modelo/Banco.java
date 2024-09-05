3295870535e4cdc41a289fd86ac37c58d425ad52
Boris Perez <borisperezg@ufps.edu.co>
2024-09-05 09:18:33 -0500

    Ajustando

--- src/exceptions/complejo/modelo/Banco.java
+++ src/exceptions/complejo/modelo/Banco.java
@@ -63,55 +63,8 @@ public class Banco {
         return lista;
     }
     
-    public void retirar(String cuenta, String valor) throws Exception{
-        
-        if(validarSiCuentaExiste(cuenta)){
-            
-            // Acá se puede generar una excepción.
-            // Qué propone hacer?
-            double dValor = Double.parseDouble(valor);
-            
-            boolean hayFondos = validarSiHayFondos(cuenta, dValor);
-            if(hayFondos){
-                // Realiza las operaciones respectivas
-            }else{
-                throw new Exception("No cuenta con fondos suficientes");
-            }
-        }else
-            throw new Exception("La cuenta no existe");
-    }
     
-    /**
-     * Este metodo simula la validación de fondos en la
-     * cuenta bancaria. Implementa un random para 
-     * retornar falso o verdadero.
-     * @param cuenta
-     * @return 
-     */
-    private boolean validarSiHayFondos(String cuenta, double valorRetirar){
-        boolean hayFondos = false;
-        
-        int valor = (int)(Math.random()*100);
-        if(valor>60)
-            hayFondos = true;
-        
-        return hayFondos;
-    }
     
-    /**
-     * Este metodo simula la validación de la cuenta. 
-     * Implementa un random para retornar falso o verdadero.
-     * @param cuenta
-     * @return 
-     */
-    private boolean validarSiCuentaExiste(String cuenta){
-        boolean existeCuenta = false;
-        
-        int valor = (int)(Math.random()*100);
-        if(valor>60)
-            existeCuenta = true;
-        
-        return existeCuenta;
-    }
+    
     
 }
--- src/exceptions/simple/ExceptionDemo.java
+++ src/exceptions/simple/ExceptionDemo.java
@@ -16,12 +16,23 @@ public class ExceptionDemo {
         ExceptionDemo de = new ExceptionDemo();
         String numero = JOptionPane.showInputDialog("Ingrese un "
                 + "valor a tranferir:");
-        de.ejemplificarSinException(numero);
-        // Si en el llamado anterior hay excepción
-        // esto no se ejecuta cuando hay excepción   
+        
+        
+        try{
+            de.ejemplificarSinException(numero);
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        
+        
+        
         de.ejemplificarConException(numero);     
         // 2do tema a resolver: Pero el main no se enteró. 
         // Se enteró fue la consola.
+        
+        // Si en el llamado anterior hay excepción
+        // esto no se ejecuta cuando hay excepción   
+        
     }
     
     public void ejemplificarConException(String num){
@@ -31,16 +42,32 @@ public class ExceptionDemo {
             System.out.println("Operación exitosa.");
         } catch (Exception e) {
             // 1er tema a resolver
-            System.out.println("O.o se produjo un error. "
-                    + "Qué clase de error?"); 
+            System.out.println("O.o se produjo un error: "
+                    + e.getMessage()); 
 //            e.printStackTrace();
         }
         System.out.println("Cerrando sesión con el banco");
     }   
     
-    public void ejemplificarSinException(String num){
+    public void ejemplificarSinException(String num) throws Exception{
         System.out.println("Abriendo sesión con el banco");        
-        int numero = Integer.parseInt(num);
+        
+        if(num.isEmpty())
+            throw new Exception("El campo 'num' no puede estar vacio.");
+        
+        int numero = 0;
+        try{
+            numero = Integer.parseInt(num);
+        }catch(Exception e){
+            // cerrar conexion a la BD
+            // limpio buffers
+            throw new Exception("El campo 'num' no puede "
+                    + "tener caracteres textuales.");
+        }
+        
+        if(numero<18)
+            throw new Exception("El número no puede ser menor a 18.");
+        
         System.out.println("Operación exitosa.");
         System.out.println("Cerrando sesión con el banco");
     } 
